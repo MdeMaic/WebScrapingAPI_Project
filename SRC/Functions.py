f0954d7cda356b90d86f2f7c8090c1f58d8758bf
MdeMaic <miguel.ceinos@gmail.com>
2020-02-03 14:56:29 +0100

    Creating argparse options

---
+++ SRC/Functions.py
@@ -0,0 +1,39 @@
+#IMPORTs
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+import re
+from bs4 import BeautifulSoup
+import json
+import requests
+from IPython.display import Image
+import os
+
+def requestJSON(url):
+    res = requests.get(url)
+    if res.status_code != 200:
+        print(res.text)
+        raise ValueError("Bad Response")
+    return res.json()
+
+def missedCountries(df):
+    found=[]
+    for ind,value in enumerate(df.Country):
+        for elem in lista_name:
+            if value == elem:
+                found.append(ind)
+    nofound=[]
+    for i in range(189):
+        if i not in found:
+            nofound.append(i)
+    noname=[]
+    for i in nofound:
+        noname.append(df.Country[i])
+    
+    if len(nofound) == 0:
+        return "No values lost"
+    else:
+        return list(zip(nofound,noname))
+
+
+
